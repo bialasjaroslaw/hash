0486e104e48225b209791150bf79ce17c0d8e9f1
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-24 23:19:05 +0100

    Rename test file

--- tests/test_md5.cpp
+++ tests/test_md5.cpp
@@ -6,7 +6,6 @@
 
 #include <hash/md5.h>
 
-namespace fs = std::filesystem;
 using namespace ::testing;
 
 TEST(Md5, ByteSwapOnBigEndian)
@@ -30,7 +29,7 @@ TEST(Md5, EmptyString)
     md5_hasher::from_string(input, result);
     uint8_t expected[] = {0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04, 0xe9, 0x80, 0x09, 0x98, 0xec, 0xf8, 0x42, 0x7e};
     for(int idx = 0 ; idx < 16 ; ++idx)
-        EXPECT_THAT(expected[idx], Eq(result[idx]));
+        EXPECT_THAT(result[idx], Eq(expected[idx]));
 }
 
 TEST(Md5, SimpleString)
@@ -40,7 +39,7 @@ TEST(Md5, SimpleString)
     md5_hasher::from_string(input, result);
     uint8_t expected[] = {0x65, 0xa8, 0xe2, 0x7d, 0x88, 0x79, 0x28, 0x38, 0x31, 0xb6, 0x64, 0xbd, 0x8b, 0x7f, 0x0a, 0xd4};
     for(int idx = 0 ; idx < 16 ; ++idx)
-        EXPECT_THAT(expected[idx], Eq(result[idx]));
+        EXPECT_THAT(result[idx], Eq(expected[idx]));
 }
 
 TEST(Md5, QuickBrownFox)
@@ -50,5 +49,5 @@ TEST(Md5, QuickBrownFox)
     md5_hasher::from_string(input, result);
     uint8_t expected[] = {0xe4, 0xd9, 0x09, 0xc2, 0x90, 0xd0, 0xfb, 0x1c, 0xa0, 0x68, 0xff, 0xad, 0xdf, 0x22, 0xcb, 0xd0};
     for(int idx = 0 ; idx < 16 ; ++idx)
-        EXPECT_THAT(expected[idx], Eq(result[idx]));
+        EXPECT_THAT(result[idx], Eq(expected[idx]));
 }
